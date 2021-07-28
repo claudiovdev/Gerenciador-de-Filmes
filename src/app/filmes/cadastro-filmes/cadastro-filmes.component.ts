84a17d04debc45927b351ff4800c7d424115c6be
viniciuscfn <claudio.vdev@gmail.com>
2021-07-28 10:03:00 -0300

    terminando os erros do formulário

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup,Validators } from '@angular/forms';
 
 @Component({
   selector: 'dio-cadastro-filmes',
@@ -8,17 +8,38 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 })
 export class CadastroFilmesComponent implements OnInit {
 
-  options: FormGroup;
+  cadastro: FormGroup;
 
   constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {
-
-    this.options = this.fb.group({
-      hideRequired: false,
-      floatLabel: 'auto',
-    });
-
+  get f(){
+    return this.cadastro.controls;
   }
 
+  public ngOnInit(): void {
+    this.cadastro = this.fb.group({
+      titulo: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(256)]],
+      urlFoto: ['', [Validators.minLength(10)]],
+      dtLancamento: ['', [Validators.required]],
+      descricao: ['', [Validators.required]],
+      nota: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
+      urlIMDb: ['', [Validators.minLength(10)]],
+      genero: ['', [Validators.required]]
+      /* Aqui nós criamos as as regras onde definimos quais campos seriam obrigatórios */
+    });
+  }
+    salvar(): void {
+      this.cadastro.markAllAsTouched();
+      if(this.cadastro.invalid){
+        return;
+      }
+      // markAllAsTouched explicação:
+      // o markAllAsTouched passa a informação de que todas minhas opções no formulário foram clicadas ou estão sujas
+
+      alert('SUCESSO!! /n/n' + JSON.stringify(this.cadastro.value, null, 4))
+    }
+
+    reiniciarForm(): void{
+      this.cadastro.reset();
+    }
 }
